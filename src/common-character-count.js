910311277db43ec5bd41fac91f855e31e3256972
Rustam Gilmutdinov <rukavina_sb@mail.ru>
2025-02-04 21:49:45 +0500

    feat: complete first 5 tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  let str = s1.concat(s2);
+  let arr = str.split('');
+
+  let sorted = arr.sort((a, b) => a.localeCompare(b));
+
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] === sorted[i + 1]) {
+      count++
+      sorted.splice(i, i + 1);
+    }
+  }
+  return count;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const str = String(n);
+  let sliced = str.slice(1);
+  return Number(sliced);
 }
 
 module.exports = {
